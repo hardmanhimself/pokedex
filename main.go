b0bffa6f98c9ed327e72466631b43f0775027562
JD Hardman <josh.hardman98@gmail.com>
2025-07-17 14:33:05 +0100

    cache

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"time"
+
+	"github.com/hardmanhimself/pokedexcli/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
+	cfg := &config{
+		caughtPokemon: map[string]pokeapi.Pokemon{},
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
